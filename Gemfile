4a9bb5db4104158820b62fdef57cbf2b2d1cee84
Jake <jnickoloff@gmail.com>
2012-05-12 23:03:46 -0700

    Converted site from static html to using Sinatra.
    Media files are referenced by the html and are absent from the project.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "http://rubygems.org"
+
+gem "sinatra"
+gem "thin"
+gem "rack-test"
+gem "rspec"
+gem "cucumber"
+gem "rake"
---
+++ app.rb
@@ -0,0 +1,9 @@
+require "sinatra/base"
+
+module Church
+  class Application < Sinatra::Application
+    get "/" do
+      send_file File.join(settings.public_folder, "index.html")
+    end
+  end
+end
